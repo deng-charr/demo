a86e23b9700a2980735f4b818ee8fa7c403b171c
xiaodeng <1963653425@qq.com>
2019-12-06 09:51:27 +0800

    qq

---
+++ qq.java
@@ -0,0 +1,10 @@
+class helloWorld{
+
+　　public static void main(String ags[]){
+
+　　　　System.out.println("Hello World!");
+              System.out.println("1");
+              System.out.println("Hello World!");
+              System.out.println("2");
+　　}
+}
